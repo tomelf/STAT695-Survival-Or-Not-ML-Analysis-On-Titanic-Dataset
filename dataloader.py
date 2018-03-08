fc998f1c683c7fc7afd9356b12eb091b1e601f6a
tomelf <tomelf2002@gmail.com>
2018-03-08 12:06:44 -0500

    - Add features has_sibsp, has_parch, fare_group. - Add a refined dataset titanic3.csv

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 import re
+import sys
 
 def preprocess(data):
     for idx, d in enumerate(data):
@@ -11,6 +12,59 @@ def preprocess(data):
         Sex = (0 if Sex == "male" else 1)
         data[idx] = [PassengerId,Pclass,Name,last_name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked]
     return np.array(data)
+
+def preprocess2(data):
+    for idx, d in enumerate(data):
+        pclass,name,sex,age,sibsp,parch,ticket,fare,cabin,embarked,boat,body,dest = d;
+        # get last name
+        name_objs = re.match( r'([^,]*)', name)
+        last_name = name_objs.group(1).strip()
+        sex = (0 if sex == "male" else 1)
+        has_sibsp = 1 if int(sibsp) > 0 else 0
+        has_parch = 1 if int(parch) > 0 else 0
+        data[idx] = [pclass,name,last_name,sex,age,sibsp,has_sibsp,parch,has_parch,ticket,fare,cabin,embarked,boat,body,dest]
+    np_data_fare = np.array(data)[:,10]
+    np_data_fare[np_data_fare==""] = "0"
+    np_data_fare = np_data_fare.astype('float')
+    fare_mean = np.mean(np_data_fare)
+    fare_std = np.std(np_data_fare)
+    group_bounds = [fare_mean-2*fare_std, fare_mean-fare_std, fare_mean, fare_mean+fare_std, fare_mean+2*fare_std]
+    for idx, d in enumerate(data):
+        data[idx][10] = 0 if data[idx][10]=="" else float(data[idx][10])
+        fare = data[idx][10]
+        fare_group = -1
+        for i in range(len(group_bounds)):
+            if i==0:
+                b1 = 0
+                b2 = group_bounds[i]
+            elif i==len(group_bounds)-1:
+                b1 = group_bounds[i]
+                b2 = sys.float_info.max
+            else:
+                b1 = group_bounds[i]
+                b2 = group_bounds[i+1]
+            if b1<=fare<b2:
+                fare_group = i
+                break
+        data[idx].insert(11, fare_group)
+    return np.array(data)
+
+def load_all_data():
+    test_data_path = "dataset/titanic3.csv"
+
+    X_test = []
+    y_test = []
+    with open(test_data_path, 'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for idx, row in enumerate(spamreader):
+            if idx > 0:
+                pclass,survived,name,sex,age,sibsp,parch,ticket,fare,cabin,embarked,boat,body,dest = row
+                X_test.append([pclass,name,sex,age,sibsp,parch,ticket,fare,cabin,embarked,boat,body,dest])
+                y_test.append(survived)
+
+    X_test = preprocess2(X_test)
+
+    return X_test, y_test
 
 def load_testing_data():
     test_data_path = "dataset/test.csv"
--- task1.py
+++ task1.py
@@ -8,17 +8,48 @@ from sklearn.model_selection import cross_val_score
 
 import dataloader
 
+def get_feature_index_list(str_f_list):
+    str_feature_list = "pclass,name,last_name,sex,age,sibsp,has_sibsp,parch,has_parch,ticket,fare,fare_group,cabin,embarked,boat,body,dest"
+    feature_list = str_feature_list.split(",")
+    f_list = str_f_list.split(",")
+    return [feature_list.index(f) for f in f_list]
+
 def main():
-
-    X, y = dataloader.load_training_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.1)
+    X, y = dataloader.load_all_data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=(418/(418+891)))
     print("Number of training examples: {}, testing examples: {}".format(len(X_train), len(X_test)))
-    # Origianl features: PassengerId,Pclass,Name,last_name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
-    X_train = X_train[:,[1,6,7]]
+    # Origianl features: pclass,name,last_name,sex,age,sibsp,has_sibsp,parch,has_parch,ticket,fare,fare_group,cabin,embarked,boat,body,dest
 
-    clf = svm.SVC(kernel='rbf', C=1, gamma='auto', probability=False)
-    scores = cross_val_score(clf, X_train, y_train, cv=5)
-    print("5-fold cross validation: {}, mean: {}".format(scores, np.mean(scores)))
+    testing_feature_sets = [
+        "pclass",
+        "sex",
+        "sibsp",
+        "parch",
+        "fare",
+        "pclass,sex,sibsp,parch,fare",
+        "pclass,sex,sibsp,parch,fare,has_sibsp,has_parch,fare_group"
+    ]
+
+    for testing_feature_set in testing_feature_sets:
+        print("Used feature set: {}".format(testing_feature_set))
+        f_idxes = get_feature_index_list(testing_feature_set)
+        X_train_filtered = X_train[:,f_idxes]
+        X_test_filtered = X_test[:,f_idxes]
+
+        # print("Example of original: {}".format(X_train[0]))
+        # print("Example of filtered: {}".format(X_train_filtered[0]))
+
+        clf = svm.SVC(kernel='rbf', C=1, gamma='auto', probability=False)
+
+        # scores = cross_val_score(clf, X_train_filtered, y_train, cv=5)
+        # print("5-fold cross validation: mean accuracy: {}, accuracy of each folders: {}".format(np.mean(scores), scores))
+
+        clf.fit(X_train_filtered, y_train)
+        print("Training accuracy: {}, Testing accuracy: {}\n".format(
+                clf.score(X_train_filtered, y_train),
+                clf.score(X_test_filtered, y_test)
+            )
+        )
 
 if __name__ == "__main__":
     main()
